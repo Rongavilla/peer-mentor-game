1cf5d249b089ce3a9be9364b3d88fbb321e5efea
Randell Halili Rongavilla <143256869+Rongavilla@users.noreply.github.com>
2025-10-08 18:04:47 +0800

    Add Phaser game configuration file

---
+++ src/game/config.ts
@@ -0,0 +1,24 @@
+import Phaser from 'phaser'
+import { MenuScene } from './scenes/MenuScene'
+import { WorldScene } from './scenes/WorldScene'
+import { BattleScene } from './scenes/BattleScene'
+
+export const gameConfig: Phaser.Types.Core.GameConfig = {
+  type: Phaser.AUTO,
+  parent: 'game-container',
+  width: 1280,
+  height: 720,
+  backgroundColor: '#1a1a2e',
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 0 },
+      debug: false,
+    },
+  },
+  scene: [MenuScene, WorldScene, BattleScene],
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
+}
